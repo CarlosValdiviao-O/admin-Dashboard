bd97222bffef157294b751c31fdcf9a54741b2d4
CarlosValdiviao-O <carlosvaldivia390@gmail.com>
2022-04-27 13:14:40 -0400

    header elements with responsive design added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const icon = document.getElementsByClassName('icon');
 const link = document.getElementsByClassName('link');
 const arr = Array.from(link);
-for (i=0; i<icon.length; i++){
-    link[i].addEventListener('mouseenter', (e) =>{
+for (i=0; i<link.length; i++){
+    arr[i].addEventListener('mouseenter', (e) =>{
     let i = arr.findIndex((element)=> element.textContent == e.target.textContent);
     icon[i].classList.add('hovered'); 
     if(i==0) icon[i].classList.remove('hovered');   
@@ -13,3 +13,17 @@ for (i=0; i<icon.length; i++){
     });
 };
 
+let bell = document.getElementById('bell');
+bell.addEventListener('click', toggleBell);
+let bellAux = 0;
+
+function toggleBell() {
+    if (bellAux == 0){
+    bell.lastChild.src = 'icons/bell.svg';
+    bellAux++;
+    }
+    else {
+    bell.lastChild.src = 'icons/bell-outline.svg';
+    bellAux--;
+    }
+}
